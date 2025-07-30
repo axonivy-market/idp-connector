e4f724ff55ff4d2fd24c37b7cd0b237471864a36
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-07-30 21:26:00 +0700

    update workflow

--- idp-connector-test/src_test/com/axonivy/connector/idp/test/constants/IdpTestConstants.java
+++ idp-connector-test/src_test/com/axonivy/connector/idp/test/constants/IdpTestConstants.java
@@ -5,5 +5,5 @@ public class IdpTestConstants {
 	public static final String REAL_CALL_CONTEXT_DISPLAY_NAME = "Real Server Test";
 	public static final String API_KEY_SECRET = "apiKeySecret";
 	public static final String END_TO_END_TESTING_ENVIRONMENT_KEY = "testEnvironment";
-	public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E Testing"
+	public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E"
 }
