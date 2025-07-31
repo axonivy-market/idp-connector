0c26c18d350c056f62cae0a955cafdb5df6c7e00
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-07-31 11:17:52 +0700

    fix typo

--- idp-connector-test/src_test/com/axonivy/connector/idp/test/context/TestEnironmentInvocationContext.java
+++ idp-connector-test/src_test/com/axonivy/connector/idp/test/context/TestEnironmentInvocationContext.java
@@ -10,10 +10,10 @@ import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
 import org.junit.jupiter.api.extension.TestTemplateInvocationContext;
 
-public class TestEnironmentInvocationContext implements TestTemplateInvocationContext {
+public class TestEnVironmentInvocationContext implements TestTemplateInvocationContext {
 	private String contextDisplayName;
 
-	public TestEnironmentInvocationContext(String contextDisplayName) {
+	public TestEnVironmentInvocationContext(String contextDisplayName) {
 		this.contextDisplayName = contextDisplayName;
 	}
 
