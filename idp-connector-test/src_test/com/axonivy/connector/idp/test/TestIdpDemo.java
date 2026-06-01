1c6a623b0e7b8524fce1a0f9c3ffaf01590e8c83
Dinh Nguyen <127725498+ntqdinh-axonivy@users.noreply.github.com>
2026-06-01 08:49:35 +0700

    MARP-4446 unify rest client name (#66)
    * handle changing
    
    * udpate rest client Name
    
    * Update rest-clients.yaml

--- idp-connector-test/src_test/com/axonivy/connector/idp/test/TestIdpDemo.java
+++ idp-connector-test/src_test/com/axonivy/connector/idp/test/TestIdpDemo.java
@@ -16,17 +16,11 @@ import ch.ivyteam.ivy.security.ISession;
 @IvyProcessTest(enableWebServer = true)
 @ExtendWith(MultiEnvironmentContextProvider.class)
 public class TestIdpDemo extends BaseSetup {
-	private static final String REST_UUID = "c316f4d1-daa6-4ca2-b3e0-68133e54eb99";
 
 	@TestTemplate
 	public void testOrganizations(BpmClient bpmClient, ISession session, AppFixture fixture, IApplication app) {
 		ExecutionResult result = bpmClient.start().process("IDPDemo/workflows.ivp").execute();
 		com.axonivy.connector.idp.connector.demo.Data data = result.data().last();
 		assertThat(data.getWorkflows()).isNotEmpty();
-	}
-
-	@Override
-	public String getUuid() {
-		return REST_UUID;
 	}
 }
